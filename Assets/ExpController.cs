591bb548f11ccbb044dbc86c2cb94bafb56ad8a7
wen-jie.tseng <wjdomain@host12.hci.informatik.tu-darmstadt.de>
2024-03-05 10:06:40 +0100

    working virtual trial

--- Assets/ExpController.cs
+++ Assets/ExpController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using TMPro;
+using System.IO;
 
 public class ExpController : MonoBehaviour
 {
@@ -24,9 +26,54 @@ public class ExpController : MonoBehaviour
     public GameObject stripesVirtualTarget;
     public List<GameObject> physicalTargetList;
     public List<GameObject> virtualTargetList;
+    // for procedures
+    public GameObject StartTrialPanel;
+
+    private AudioSource pointingIndicator;
+
+
+    [Header("Variables and Trials")]
+    public int layoutBlockNum = 0; // 4 layouts of physical pairs
+    public enum PhyTargetsLayouts {A = 0, B = 1, C = 2, D = 3};
+    public PhyTargetsLayouts currentPhyTargetsLayout;
+
+    public int conditionBlockNum = 0; // condition block repeat 3 times, each includs four combinations
+    public enum Conditions {durationShort_0 = 0, durationShort_120 = 1, durationLong_0 = 2, durationLong_120 = 3};
+    public Conditions currentCondition;
+    public List<int> conditionArray;
+
+    public int trialNum = 0; // 0-4 (4 trials per condition block), 4 x 3 x 4 = 48 total
+    public float currentTime = 0;
+    bool isTrialRunning;
+    bool isBaselineMeasure;
+    bool isTestingMeasure;
+    float beginTimeStamp;
+    float endTimeStamp;
+    [Header("Pointing")]
+    public GameObject controller; 
+    public LayerMask pointingMask;
+    public GameObject laser;
+    private Transform laserTransform;
+    public Vector3 hitPoint;
+    public List<GameObject> virtualTargetReticle;
+    public List<GameObject> physicalTargetReticle;
+    Ray ray;
+    RaycastHit hit;
+
+    public enum SelfRotation {none = 0, rotate = 1};
+    public SelfRotation currentRotation;
+    public enum Targets {virtualTarget = 0, physicalTarget = 1}; // see the procedure :D
+    public Targets currentTarget;
+    [Header("UI")]
+
+    public GameObject instruction;
+    public TMP_Text instructionText;
+
     
     void Start()
     {
+
+        pointingIndicator = GetComponent<AudioSource>();
         Helpers.Shuffle(physicalTargetList);
         Helpers.Shuffle(virtualTargetList);
         bluePhysicalTarget.SetActive(false);
@@ -46,6 +93,8 @@ public class ExpController : MonoBehaviour
             InitializeVirtualTargets();
         }
         
+        // update current time
+
     }
 
     void InitializeVirtualTargets()
@@ -69,5 +118,30 @@ public class ExpController : MonoBehaviour
         stripesVirtualTarget.SetActive(true);
         dottedVirtualTarget.transform.position = dottedPos;
         stripesVirtualTarget.transform.position = stripesPos;
+    }
+
+    public IEnumerator RunOneVirtualTrial(float _showTimeStamp = 5.0f, float _retentionTimeStamp = 10.0f, float rotation_amount = 0f)
+	{
+        currentTime = 0.0f;
+        instructionText.text = "";
+        StartTrialPanel.SetActive(false);
+        // show a target
+        InitializeVirtualTargets();
+
+        yield return new WaitUntil(() => currentTime > _showTimeStamp);
+
+        // add fade in transparent for smoothness
+        dottedVirtualTarget.SetActive(false);
+        stripesVirtualTarget.SetActive(false);
+
+        yield return new WaitUntil(() => currentTime > _retentionTimeStamp);
+
+        // prepare for the pointing task
+        beginTimeStamp = currentTime;
+        pointingIndicator.Play();
+
+        laser.SetActive(true);
+        isBaselineMeasure = true;
+        yield return 0;
     }
 }
