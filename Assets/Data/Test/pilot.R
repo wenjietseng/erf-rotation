724f942129308f4c9b720cb9e4b8f3fcd444ec30
wen-jie.tseng <wjdomain@wen-jietsengs-MacBook-Pro.local>
2024-04-18 09:52:22 +0200

    update pilot data analysis

--- Assets/Data/Test/pilot.R
+++ Assets/Data/Test/pilot.R
@@ -11,11 +11,12 @@
 # - move TargetName to the other factor variables
 
 ## load packages
+library(Rmisc) # plyr and dplyr imcompatible. Need to look into code implementation
 library(dplyr)
 library(ggplot2)
 
 ## get pilot data
-setwd("Documents/erf-rotation/Assets/Data/Test/")
+# setwd("Documents/erf-rotation/Assets/Data/Test/")
 
 # y
 # conditions and layouts (5,6,7) have some mistakes. Need to check that
@@ -148,6 +149,8 @@ dta_clean2 |> ggplot(aes(x=RT, y=distErr, color=SelfRotation)) +
   facet_grid(TargetType ~ .) +
   theme_bw()
 
+str(dta_clean2)
+
 # Baseline vs. Testing
 # mean calculation
 dta_clean.mean <- dta_clean2 |> group_by(Participant, Baseline) |>
@@ -155,22 +158,22 @@ dta_clean.mean <- dta_clean2 |> group_by(Participant, Baseline) |>
             distErr.mean = mean(distErr))
 
 ggplot(dta_clean2, aes(x=RT, fill=Baseline)) +
-  geom_histogram(aes(y=..density..), color= "black", alpha=0.2, position='identity', binwidth=.200) +
+  geom_histogram(aes(y=after_stat(density)), color= "black", alpha=0.2, position='identity', binwidth=.200) +
   geom_density(alpha=.2) +
   geom_vline(data=dta_clean.mean, aes(xintercept=RT.mean, color=Baseline),
             linetype="dashed", linewidth=1) +
   facet_grid(Participant~.) +
-  scale_fill_manual(values=c("red", "steelblue", "purple")) +
-  scale_color_manual(values=c("red", "steelblue", "purple")) 
+  scale_fill_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) 
 
 ggplot(dta_clean2, aes(x=distErr, fill=Baseline)) +
-  geom_histogram(aes(y=..density..), color= "black", alpha=0.4, position='identity', binwidth=.05) +
+  geom_histogram(aes(y=after_stat(density)), color= "black", alpha=0.2, position='identity', binwidth=.05) +
   geom_density(alpha=.2) +
   geom_vline(data=dta_clean.mean, aes(xintercept=distErr.mean, color=Baseline),
              linetype="dashed", linewidth=1) +
   facet_grid(Participant~.) +
-  scale_fill_manual(values=c("red", "steelblue", "purple")) +
-  scale_color_manual(values=c("red", "steelblue", "purple")) 
+  scale_fill_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) 
 
 #
 dta_clean.cond <- dta_clean |> group_by(Participant, Baseline, Condition) |>
@@ -178,131 +181,225 @@ dta_clean.cond <- dta_clean |> group_by(Participant, Baseline, Condition) |>
             distErr.mean = mean(distErr))
 
 ggplot(dta_clean2, aes(x=RT, fill=Condition)) +
-  geom_histogram(aes(y=..density..), color= "black", alpha=0.4, position='identity', binwidth=.2) +
+  geom_histogram(aes(y=after_stat(density)), color= "black", alpha=0.2, position='identity', binwidth=.2) +
   geom_density(alpha=.2) +
   geom_vline(data=dta_clean.cond, aes(xintercept=RT.mean, color=Condition),
-  linetype="dashed", size=1) +
+  linetype="dashed", linewidth=1) +
   facet_grid(Participant~Baseline) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) 
+  scale_fill_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) 
 
 ggplot(dta_clean, aes(x=distErr, fill=Condition)) +
-  geom_histogram(aes(y=..density..), color= "black", alpha=0.4, position='identity', binwidth=.05) +
+  geom_histogram(aes(y=after_stat(density)), color= "black", alpha=0.2, position='identity', binwidth=.05) +
   geom_density(alpha=.2) +
   geom_vline(data=dta_clean.cond, aes(xintercept=distErr.mean, color=Condition),
-             linetype="dashed", size=1) +
+             linetype="dashed", linewidth=1) +
   facet_grid(Participant~Baseline) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) 
+  scale_fill_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) 
 #
+
+
+## Don't forget to summarize within-participants data using method from Morey (2008).
+# https://www.rdocumentation.org/packages/Rmisc/versions/1.5.1/topics/summarySEwithin
+
+
+# https://gist.github.com/hauselin/a83b6d2f05b0c90c0428017455f73744#file-summarysewithin2-r
+ci.rt.by.decoy <- summarySEwithin(data = dta_clean2,
+                                     measurevar = "RT",
+                                     withinvars = "DecoyAmount",
+                                     idvar = "Participant")
+ci.dist.by.decoy <- summarySEwithin(data = dta_clean2,
+                                         measurevar = "distErr",
+                                         withinvars = "DecoyAmount",
+                                         idvar = "Participant")
 
 ## Check if three decoys mess up people's performance badly
 # three decoys vs. two decoys
 dta_clean2 |> ggplot(aes(x=DecoyAmount, y=RT)) +
   stat_summary(fun = mean, geom = "point", size = 4) +
-  stat_summary(fun.data = mean_se, geom = "errorbar",
-               linetype = "solid", width = .2) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) 
+  geom_errorbar(data=ci.rt.by.decoy,
+                aes(ymin = RT - ci, ymax = RT + ci), width = .2) +
+  theme_bw()
+
+# dta_clean2 |> ggplot(aes(x=DecoyAmount, y=RT)) +
+#   stat_summary(fun = mean, geom = "point", size = 4) +
+#   stat_summary(fun.data = mean_cl_boot, geom = "errorbar",
+#                linetype = "solid", width = .2) +
+#   theme_bw()
+
 
 dta_clean2 |> ggplot(aes(x=DecoyAmount, y=distErr)) +
   stat_summary(fun = mean, geom = "point", size = 4) +
-  stat_summary(fun.data = mean_se, geom = "errorbar",
-               linetype = "solid", width = .2) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) 
+  geom_errorbar(data=ci.dist.by.decoy,
+                aes(ymin = distErr - ci, ymax = distErr + ci), width = .2) +
+  theme_bw()
 
 
-dta_by_p <- dta_clean2 |> group_by(Participant, Baseline, Condition) |>
-  summarise(rt_m = mean(RT),
-            dist_m = mean(distErr))
-  
-levels(dta_by_p$Baseline) <- c("Testing", "Baseline")
+################################# Baseline
 
-dta_by_p |>
-  ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$rt_m, mean), y=rt_m)) +
-  stat_summary(fun = mean, geom = "point", size = 4) +
-  stat_summary(fun.data = mean_se, geom = "errorbar",
-               linetype = "solid", width = .2) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan"))
+baseline.ci.rt <- summarySEwithin(data = dta_clean2,
+                                   measurevar = "RT",
+                                   withinvars = c("Baseline"),
+                                   idvar = "Participant")
 
-# RT summarized by participants
-dta_by_p |>
-  ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$rt_m, mean), y=rt_m,
-             shape=Condition, group=Condition)) +
-  stat_summary(fun = mean, geom = "point", size = 2,
-               position = position_dodge(width=.2)) +
-  stat_summary(fun.data = mean_se, geom = "pointrange", linetype = "solid",
-               position = position_dodge(width=.2)) +
-  stat_summary(fun.y = mean, geom = "line",
-               position = position_dodge(width=.2),
-               linetype="dashed", size=.5) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) +
+baseline.ci.dist <- summarySEwithin(data = dta_clean2,
+                                     measurevar = "distErr",
+                                     withinvars = c("Baseline"),
+                                     idvar = "Participant")
+#
+baseline.ci.rt |>
+  ggplot(aes(x=reorder(Baseline, RT, mean),
+             y=RT)) +
+    geom_pointrange(aes(ymin = RT - ci, ymax = RT + ci)) +
+    labs(x="Response Type", y="Reaction Time (s)") +
+    theme_bw()
+
+baseline.ci.dist |>
+  ggplot(aes(x=reorder(Baseline, distErr, mean),
+             y=distErr)) +
+  geom_pointrange(aes(ymin = distErr - ci, ymax = distErr + ci)) +
+  labs(x="Response Type", y="Absolut Position Error (m)") +
+  theme_bw()
+
+#################################
+
+base.cond.ci.rt <- summarySEwithin(data = dta_clean2,
+                                   measurevar = "RT",
+                                   withinvars = c("Condition", "Baseline"),
+                                   idvar = "Participant")
+
+base.cond.ci.dist <- summarySEwithin(data = dta_clean2,
+                                     measurevar = "distErr",
+                                     withinvars = c("Condition", "Baseline"),
+                                     idvar = "Participant")
+
+
+# RT summarized by conditions
+base.cond.ci.rt |>
+  ggplot(aes(x=reorder(Baseline, RT, mean), y=RT,
+             shape=Condition, group=Condition, color=Condition)) +
+  geom_pointrange(aes(ymin = RT - ci, ymax = RT + ci),
+                  position=position_dodge(width=.1)) +
+  geom_line(linetype="dashed", size=.5,
+            position=position_dodge(width=.1)) +
   scale_shape_manual(values=c(18, 17, 20, 15)) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
   labs(x="Response Type", y="Reaction Time (s)") +
-  theme_bw() 
+  theme_bw()
 
 # dist
-dta_by_p |>
-  ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$dist_m, mean), y=dist_m,
-             shape=Condition, group=Condition)) +
-  stat_summary(fun = mean, geom = "point", size = 2,
-               position = position_dodge(width=.2)) +
-  stat_summary(fun.data = mean_se, geom = "pointrange", linetype = "solid",
-               position = position_dodge(width=.2)) +
-  stat_summary(fun.y = mean, geom = "line",
-               position = position_dodge(width=.2),
-               linetype="dashed", size=.5) +
-  scale_fill_manual(values=c("red", "steelblue", "purple", "cyan")) +
-  scale_color_manual(values=c("red", "steelblue", "purple", "cyan")) +
+base.cond.ci.dist |>
+  ggplot(aes(x=reorder(Baseline, distErr, mean), y=distErr,
+             shape=Condition, group=Condition, color=Condition)) +
+  geom_pointrange(aes(ymin = distErr - ci, ymax = distErr + ci),
+                  position=position_dodge(width=.1)) +
+  geom_line(linetype="dashed", size=.5,
+            position=position_dodge(width=.1)) +
   scale_shape_manual(values=c(18, 17, 20, 15)) +
+  scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
   labs(x="Response Type", y="Absolut Position Error (m)") +
-  theme_bw() 
+  theme_bw()
 
-# summary statistics
-dta_by_p |> group_by(Condition, Baseline) |>
-  summarise(rt.m = mean(rt_m),
-            rt.sd = sd(rt_m),
-            dist.m = mean(dist_m),
-            dist.sd = sd(dist_m))
+
+###
+# dta_by_p <- dta_clean2 |> group_by(Participant, Baseline, Condition) |>
+#   summarise(rt_m = mean(RT),
+#             dist_m = mean(distErr))
+# 
+# levels(dta_by_p$Baseline) <- c("Testing", "Baseline")
+
+# dta_by_p |>
+#   ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$rt_m, mean), y=rt_m)) +
+#     stat_summary(fun = mean, geom = "point", size = 4) +
+#     stat_summary(fun.data = mean_cl_boot, geom = "errorbar",
+#                  linetype = "solid", width = .2) +
+#     labs(x="Response Type", y="Reaction Time (s)") +
+#     theme_bw()
+# 
+# dta_by_p |>
+#   ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$dist_m, mean), y=dist_m)) +
+#   stat_summary(fun = mean, geom = "point", size = 4) +
+#   stat_summary(fun.data = mean_cl_boot, geom = "errorbar",
+#                linetype = "solid", width = .2) +
+#   labs(x="Response Type", y="Absolut Position Error (m)") +
+#   theme_bw()
+
+
+# RT summarized by conditions
+# dta_by_p |>
+#   ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$rt_m, mean), y=rt_m,
+#              shape=Condition, group=Condition)) +
+#   stat_summary(fun = mean, geom = "point", size = 2,
+#                position = position_dodge(width=.2)) +
+#   stat_summary(fun.data = mean_cl_boot, geom = "pointrange", linetype = "solid",
+#                position = position_dodge(width=.2)) +
+#   stat_summary(fun.y = mean, geom = "line",
+#                position = position_dodge(width=.2),
+#                linetype="dashed", size=.5) +
+#   scale_shape_manual(values=c(18, 17, 20, 15)) +
+#   scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+#   labs(x="Response Type", y="Reaction Time (s)") +
+#   theme_bw() 
+
+# dist
+# dta_by_p |>
+#   ggplot(aes(x=reorder(dta_by_p$Baseline, dta_by_p$dist_m, mean), y=dist_m,
+#              shape=Condition, group=Condition)) +
+#   stat_summary(fun = mean, geom = "point", size = 2,
+#                position = position_dodge(width=.2)) +
+#   stat_summary(fun.data = mean_cl_boot, geom = "pointrange", linetype = "solid",
+#                position = position_dodge(width=.2)) +
+#   stat_summary(fun.y = mean, geom = "line",
+#                position = position_dodge(width=.2),
+#                linetype="dashed", size=.5) +
+#   scale_shape_manual(values=c(18, 17, 20, 15)) +
+#   scale_color_manual(values=c("#D81B60", "#1E88E5", "#FFC107", "#004D40")) +
+#   labs(x="Response Type", y="Absolut Position Error (m)") +
+#   theme_bw() 
+
+# summary statistics (need more update from here)
+# dta_by_p |> group_by(Condition, Baseline) |>
+#   summarise(rt.m = mean(rt_m),
+#             rt.sd = sd(rt_m),
+#             dist.m = mean(dist_m),
+#             dist.sd = sd(dist_m))
 
 # delta RT vs. Presence
-dta_by_p2 <- dta_clean2 |> group_by(Participant, Baseline, Condition, TargetType, SelfRotation) |>
-  summarise(rt_m = mean(RT),
-            dist_m = mean(distErr))
-
-levels(dta_by_p2$Baseline) <- c("Testing", "Baseline")
+# dta_by_p2 <- dta_clean2 |> group_by(Participant, Baseline, Condition, TargetType, SelfRotation) |>
+#   summarise(rt_m = mean(RT),
+#             dist_m = mean(distErr))
+# 
+# levels(dta_by_p2$Baseline) <- c("Testing", "Baseline")
 
 
 # physical static - virtual static
-a <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "none" &
-                      TargetType == "physicalTarget") 
-b <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "none" &
-                      TargetType == "virtualTarget") 
-a$deltaRT <- a$rt_m - b$rt_m
+# a <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "none" &
+#                       TargetType == "physicalTarget") 
+# b <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "none" &
+#                       TargetType == "virtualTarget") 
+# a$deltaRT <- a$rt_m - b$rt_m
+# 
+# # physical rotate - virtual rotate
+# a2 <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "rotate" &
+#                       TargetType == "physicalTarget") 
+# b2 <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "rotate" &
+#                       TargetType == "virtualTarget") 
 
-# physical rotate - virtual rotate
-a2 <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "rotate" &
-                      TargetType == "physicalTarget") 
-b2 <- dta_by_p2 |> filter(Baseline == "Testing" & SelfRotation == "rotate" &
-                      TargetType == "virtualTarget") 
 
 
-
-a2$deltaRT <- a2$rt_m - b2$rt_m
-
-dta_model <- rbind(a, a2)
-
-t.test(deltaRT ~ SelfRotation, data = dta_model, paired = TRUE)
-
-spes <- dta_q |> group_by(ParticipantID) |> summarise(m = mean(Response))
-
-dta_model$SPES <- rep(spes$m, 2)
-
-plot(dta_model$deltaRT[4:6], dta_model$SPES[4:6])
-plot(a2$rt_m, spes$m)
+# a2$deltaRT <- a2$rt_m - b2$rt_m
+# 
+# dta_model <- rbind(a, a2)
+# 
+# t.test(deltaRT ~ SelfRotation, data = dta_model, paired = TRUE)
+# 
+# spes <- dta_q |> group_by(ParticipantID) |> summarise(m = mean(Response))
+# 
+# dta_model$SPES <- rep(spes$m, 2)
+# 
+# plot(dta_model$deltaRT[4:6], dta_model$SPES[4:6])
+# plot(a2$rt_m, spes$m)
 
 # some models (n=3)
 # with presence?
